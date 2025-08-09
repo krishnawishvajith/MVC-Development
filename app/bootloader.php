eeef0913a02723ce82f876c57988d35b0b712e64
Krishna Wishvajith <krishnawishvajith@gmail.com>
2025-08-10 00:29:56 +0530

    change

--- app/bootloader.php
+++ app/bootloader.php
@@ -1,6 +1,6 @@
 <?php
-    //load libraries
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';
+//load libraries
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+require_once 'libraries/Database.php';
 ?>
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,40 +1,40 @@
 <?php
-    class Core{
-         //URL Format --> /controller/method/params
-         protected $currentController = 'Pages';
-         protected $currentMethod = 'index';
-         protected $param = [];
+class Core
+{
+    //URL Format --> /controller/method/params
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $param = [];
 
-         public function __construct(){
-            //print_r($this->getURL());
-            $url = $this->getURL();
+    public function __construct()
+    {
+        //print_r($this->getURL());
+        $url = $this->getURL();
 
-            if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
+        if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
             //if the contoroller exists, then load it
             $this->currentController = ucwords($url[0]);
 
             //unset the controller in the URL
             unset($url[0]);
-           
+
 
             //call the controller
-            require_once '../app/controllers/'.$this->currentController.'.php';
+            require_once '../app/controllers/' . $this->currentController . '.php';
 
             // Instantiate the controller 
             $this->currentController = new $this->currentController;
-
-            
-            }
-         }
-
-         public function getURL(){
-            if(isset($_GET['url'])){
-                $url = rtrim($_GET['url'], '/');
-                $url = filter_var($url, FILTER_SANITIZE_URL);
-                $url = explode('/',$url);
-
-                return $url;
-            }
-         }
+        }
     }
-?>
+
+    public function getURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            return $url;
+        }
+    }
+}
